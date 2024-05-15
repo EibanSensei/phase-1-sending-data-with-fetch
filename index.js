8cd8c005513d0f913c7c1b9aa84b0a54ea9942ce
Evan <edjones319@gmail.com>
2024-05-15 15:51:33 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Add your code here
+function submitData(name, email) {
+    const userData = {
+      name: name,
+      email: email
+    };
+  
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(userData)
+    };
+  
+    return fetch("http://localhost:3000/users", configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        appendIdToDOM(data.id);
+      })
+      .catch(error => {
+        appendErrorToDOM(error.message);
+      });
+  }
+  
+  function appendIdToDOM(id) {
+    const idElement = document.createElement("p");
+    idElement.textContent = `New ID: ${id}`;
+    document.body.appendChild(idElement);
+  }
+  
+  function appendErrorToDOM(errorMessage) {
+    const errorElement = document.createElement("p");
+    errorElement.textContent = `Error: ${errorMessage}`;
+    document.body.appendChild(errorElement);
+  }
